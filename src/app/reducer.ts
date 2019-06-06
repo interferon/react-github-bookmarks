1d0d3f8427c722ecfb0f443255f498d8acd351a7
Victor <myvictoryaremko@gmail.com>
2019-06-07 01:54:09 +0300

    adding some initial state

--- src/app/reducer.ts
+++ src/app/reducer.ts
@@ -1,6 +1,27 @@
 // @flow
 import { combineReducers } from 'redux-immutable'
 
-export const reducer = combineReducers({
+const initial_state : BookmarksState = {
 
+}
+
+type BookmarksState = {
+
+};
+
+type BookmarksReducers = {
+    search_bar: BookmarksState 
+};
+
+type BookmarkActions = {
+    type : "LIST_CREATED",
+    data : {
+        
+    }
+}
+
+export const reducer = combineReducers<BookmarksReducers, BookmarkActions>({
+    search_bar : (state, action) => {
+        return state || 0;
+    }
 })
