f15f7b6f361f834d765c05c4b867ae21751411f3
Victor <myvictoryaremko@gmail.com>
2019-06-09 15:08:37 +0300

    added initial styles

--- src/app/Bookmarks/components/board/Boards.tsx
+++ src/app/Bookmarks/components/board/Boards.tsx
@@ -16,8 +16,7 @@ type BoardsProps = {
     boards: Board[],
     on_new_board: (b: {title: string}) => void,
     on_new_board_title_change: (board_title: string) => void,
-    new_board_name: string,
-
+    new_board_name: string
 };
 
 const BoardItem = (item: BoardItem): JSX.Element => {
@@ -25,14 +24,14 @@ const BoardItem = (item: BoardItem): JSX.Element => {
 };
 
 const Board = (board: Board): JSX.Element => {
-    return <div key={board.id} style={{border: "1px solid black"}}>
+    return <BoardCont key={board.id}>
         <label>{board.title}</label>
         <ul>
             {
                 board.items.map(BoardItem)
             }
         </ul>
-    </div>
+    </BoardCont>
 };
 
 const BoardPlaceholder = (props: {
@@ -57,19 +56,31 @@ const Placeholder = styled.div`
     display: flex;
 `;
 
+const BoardCont = styled.div`
+    border: 1px solid black;
+`
+
+const BoardsContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    allign-items: center;
+`;
+
 export const Boards = (props: BoardsProps) => {
     return (
         <div>
             <h3>Boards</h3>
-            {
-                props.boards.map(Board)
-            }
-            <BoardPlaceholder
-                new_board_name={props.new_board_name}
-                placeholder={'Enter Name'}
-                on_board_add={() => props.on_new_board({title: props.new_board_name})}
-                on_new_board_name_change={props.on_new_board_title_change}
-            />
+            <BoardsContainer>
+                {
+                    props.boards.map(Board)
+                }
+                <BoardPlaceholder
+                    new_board_name={props.new_board_name}
+                    placeholder={'Enter Name'}
+                    on_board_add={() => props.on_new_board({title: props.new_board_name})}
+                    on_new_board_name_change={props.on_new_board_title_change}
+                />
+            </BoardsContainer>
         </div>
     );
 }
