b3f908dfb2c5ac445e39e86a01c5cbdd097863df
Victor <myvictoryaremko@gmail.com>
2019-06-13 20:45:45 +0300

    update

--- src/app/Bookmarks/components/board/BoardComponent.tsx
+++ src/app/Bookmarks/components/board/BoardComponent.tsx
@@ -12,6 +12,7 @@ const BoardContainer = styled.div`
     box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 3px 1px -2px rgba(0,0,0,.2),0 1px 5px 0 rgba(0,0,0,.12);
     min-width: 250px;
     margin: 20px;
+    padding: 10px;
 `
 const ItemsListContainer = styled.ul`
     min-height: 50px
